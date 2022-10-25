d5054a9ab601d3b28704b62f2026cb1818e4b864
niteenkum <niteen.kumar014@gmail.com>
2022-10-25 12:28:33 +0530

    updated proc file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,20 +1,51 @@
 
-/* Importing the mongoose module. */
-const mongoose = require('mongoose');
+// /* Importing the mongoose module. */
+// const mongoose = require('mongoose');
  
-/* Connecting to the database. */
-mongoose.connect('mongodb://localhost/polling_api');
+// /* Connecting to the database. */
+// mongoose.connect('mongodb://localhost/polling_api');
 
- /* Creating a connection to the database. */
+//  /* Creating a connection to the database. */
+// const db = mongoose.connection;
+ 
+// /* This is a callback function that is called when the connection is opened. */
+// db.on('error', console.error.bind(console, 'Error while connecting to mongo db'));
+ 
+// /* A callback function that is called when the connection is opened. */
+// db.once('open', function(){
+//     console.log('Connected to Database :: MongoDB');
+// })
+ 
+// /* Exporting the db object so that it can be used in other files. */
+// module.exports = db;
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+
+// DB URL from .env file  
+
+let DB_URL = process.env.DATABASE;
+mongoose
+  .connect(DB_URL, {
+    usenewurlparser: true,
+    useunifiedtopology: true,
+  })
+  .then(() => {
+    console.log(`connection successful `);
+  })
+  .catch((err) => {
+    console.log(`error connecting to database`, err);
+  });
+
+  
 const db = mongoose.connection;
- 
-/* This is a callback function that is called when the connection is opened. */
+
 db.on('error', console.error.bind(console, 'Error while connecting to mongo db'));
- 
-/* A callback function that is called when the connection is opened. */
+
 db.once('open', function(){
     console.log('Connected to Database :: MongoDB');
 })
- 
-/* Exporting the db object so that it can be used in other files. */
+
 module.exports = db;
